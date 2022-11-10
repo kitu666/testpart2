43aadedfb190f8222b509803b53d5bec054422e1
kitu666 <117849407+kitu666@users.noreply.github.com>
2022-11-10 16:30:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function play() {
+     document.getElementById("myaudio").play();
+}
+function play2() {
+     document.getElementById("secondaudio").play();
+}
+function play3() {
+     document.getElementById("videoplay1").play();
+
+
+}
